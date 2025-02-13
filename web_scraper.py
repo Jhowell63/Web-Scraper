37b341cf61ba6f7d8e79cbca14449bd311d9b4f5
James Howell <howelljames621@gmail.com>
2025-02-12 18:20:34 -0600

    Verson 1

--- web_scraper.py
+++ web_scraper.py
@@ -1,1 +1,17 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "https://www.newegg.ca/p/N82E16883360565?Item=N82E16883360565&cm_sp=Homepage_SS-_-P2_83-360-565-_-02122025"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+prices = doc.find_all(string="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+print(strong.string)
+
+
+
+
