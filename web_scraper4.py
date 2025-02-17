8710f8159181884ab3f4341dfb7a0314203dcd65
James Howell <howelljames621@gmail.com>
2025-02-17 17:26:13 -0600

    Second Push

--- web_scraper4.py
+++ web_scraper4.py
@@ -2,3 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+product = input("What product do you want to search for? ")
+
+url = f"https://www.newegg.ca/p/pl?d={product}&N=4131"
+page = requests.get(url).text
+doc = BeautifulSoup(page, "html.parser")
